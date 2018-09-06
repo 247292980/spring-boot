9a7ddfbe324f9442da935a60a5fd39b199ef4e83
liangguiping <liangguiping@tuandai.com>
2018-09-06 16:00:26 +0800

    rabbitmq初始化

---
+++ rabbitmq/src/main/java/com/lgp/rabbitmq/controller/HiController.java
@@ -0,0 +1,28 @@
+package com.lgp.rabbitmq.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @AUTHOR lgp
+ * @DATE 2018/7/31 15:25
+ * @DESCRIPTION
+ **/
+@RequestMapping("")
+@Controller
+public class HiController {
+    /**
+     * 返回html模板.
+     */
+    @RequestMapping("/index")
+    public String index(Map<String, Object> map) {
+        map.put("hello", "from HiController map");
+        Map<String, Object> temp = new HashMap();
+        temp.put("hi", "from HiController new map");
+        /*不用写后缀*/
+        return "/index";
+    }
+}
