26088e88306161b2a9d5ebb5f0a9828a64da3dcf
liangguiping <liangguiping@tuandai.com>
2018-10-22 11:27:47 +0800

    lazyload end

---
+++ lazyload/src/main/java/com/lgp/lazyload/LazyloadApplication.java
@@ -0,0 +1,12 @@
+package com.lgp.lazyload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyloadApplication.class, args);
+	}
+}
